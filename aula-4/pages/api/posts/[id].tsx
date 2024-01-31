9462014027fd7992b8a5ab8ea914165e5ec63524
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-01-31 12:52:56 -0300

    refact: NextApiHandler & RespondeData types on posts-api

--- aula-4/pages/api/posts/[id].tsx
+++ aula-4/pages/api/posts/[id].tsx
@@ -1,4 +1,12 @@
-export default function handler(req, res) {
-  const id = req.query.id;
+import type { NextApiHandler } from 'next';
+
+export type ResponseData = {
+  post: string;
+};
+
+const handler: NextApiHandler<ResponseData> = (req, res) => {
+  const id = req.query.id as string;
   res.status(200).json({ post: id })
 }
+
+export default handler;
